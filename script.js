62d42800bd296a348ce348b3b95ccb42b1f2ae30
AvantikaPatel <avantikakurmi2012@gmail.com>
2025-05-31 20:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+  square.addEventListener("mouseover", () => {
+    squares.forEach(
+      (s) => (s.style.backgroundColor = s === square ? "#E6E6FA" : "#6F4E37")
+    );
+  });
+
+  square.addEventListener("mouseout", () => {
+    squares.forEach((s) => (s.style.backgroundColor = "#E6E6FA"));
+  });
+});
